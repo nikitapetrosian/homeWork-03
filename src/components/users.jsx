8a52198f9513d571453c6051de775c132d955b2a
Nikita <nekitrus92@yandex.ru>
2021-08-27 21:33:36 +0300

    homeWork

--- src/components/users.jsx
+++ src/components/users.jsx
@@ -1,15 +1,12 @@
 import React, { useState } from "react";
 import api from "../api";
-// import { BootstrapTable, TableHeaderColumn } from "react-bootstrap-table";
 const Users = () => {
   const [users, setUsers] = useState(api.users.fetchAll());
-  //   console.log(users[0]);
-  //   const handleDelete = (userId)=>{ }
-  //   const randerPhrse = (number)=>{ }
   const onDelete = (id) => {
-    const newArr = users.filter((user) => user._id != id);
+    const newArr = users.filter((user) => user._id !== id);
     setUsers(newArr);
   };
+
   return (
     <>
       <h1>
@@ -25,6 +22,7 @@ const Users = () => {
             <th scope="col">Профессия</th>
             <th scope="col">Встретился, раз</th>
             <th scope="col">Оценка</th>
+            <th csope="col">Избранное</th>
             <th></th>
           </tr>
         </thead>
@@ -38,7 +36,6 @@ const Users = () => {
                   <span
                     className={`p-1 m-2 bg-${quality.color} text-white smallText `}
                     key={quality._id}
-                    // style={{ backgroundColor:  }}
                   >
                     {quality.name}
                   </span>
@@ -47,6 +44,20 @@ const Users = () => {
               <td>{user.profession.name}</td>
               <td>{user.completedMeetings}</td>
               <td>{user.rate}</td>
+              <td>
+                <button>
+                  <svg
+                    xmlns="http://www.w3.org/2000/svg"
+                    width="16"
+                    height="16"
+                    fill="currentColor"
+                    className="bi bi-bookmark-fill svg2"
+                    viewBox="0 0 16 16"
+                  >
+                    <path d="M2 2v13.5a.5.5 0 0 0 .74.439L8 13.069l5.26 2.87A.5.5 0 0 0 14 15.5V2a2 2 0 0 0-2-2H4a2 2 0 0 0-2 2z" />
+                  </svg>
+                </button>
+              </td>
               <td>
                 <button
                   className="badge bg-danger"
