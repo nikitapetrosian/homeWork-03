b29adb50e05fa964a0dfa561119932b2c2632297
Nikita <nekitrus92@yandex.ru>
2021-09-04 16:18:11 +0300

    fast-c-end

--- src/index.js
+++ src/index.js
@@ -5,10 +5,10 @@ import reportWebVitals from "./reportWebVitals";
 import "bootstrap/dist/css/bootstrap.css";
 import Users from "./components/users";
 ReactDOM.render(
-  <React.StrictMode>
-    <Users />
-  </React.StrictMode>,
-  document.getElementById("root")
+    <React.StrictMode>
+        <Users />
+    </React.StrictMode>,
+    document.getElementById("root")
 );
 
 // If you want to start measuring performance in your app, pass a function
